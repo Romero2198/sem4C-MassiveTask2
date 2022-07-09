bdfe702a3632e8c5d3a4713ce72d55dfedf01b42
Yung.Energy <morskojroma@gmail.com>
2022-07-09 17:36:38 +0300

    task2

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int a = value;
+while(value > 0)
+{
+    result+= value%10;
+    value = value/10;
+}
+Console.WriteLine($"{a}-> {result}");
